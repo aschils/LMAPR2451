72969909031573d5c5590d6c92faf62c8290d24f
Arnaud Schils <arnaud.schils@gmail.com>
2017-04-07 15:10:12 +0200

    Convergence study of etotal regarding ngkpt

--- work/ngkpt_conv.sh
+++ work/ngkpt_conv.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#convergence regarding ecut if prev_etotal - cur_etotal < delta_etotal_conv"
-delta_etotal_conv="0.0001"
+#convergence regarding ngkpt if prev_etotal - cur_etotal < delta_etotal_conv"
+delta_etotal_conv="0.001"
 
-#path of input file with current value of ecut
+#path of input file with current value of ngkpt
 temp_input_file_path="temp_input_file.in"
 
 prev_etotal="9999999"
 cur_etotal="0"
-ecut="1"
+ngkpt="1"
 
 has_converged="0"
 
-ecut_vec=""
+ngkpt_vec=""
 etotal_vec=""
 
 first_iter=true
@@ -20,9 +20,9 @@ first_iter=true
 while [ "$has_converged" -ne "1" ]
 do
 
-  output_file_path="../output/bismuth_ecut_$ecut.out"
+  output_file_path="../output/bismuth_ngkpt_$ngkpt.out"
 
-  #built abinit input, referring to temporary current .in file with one ecut value
+  #built abinit input, referring to temporary current .in file with one ngkpt value
   #and specifying corresponding output file
   abinit_files="$temp_input_file_path
 $output_file_path
@@ -31,36 +31,36 @@ tbase1_xo
 tbase1_x
 ../psps/LDA/83bi.pspnc"
 
-  #add ecut value to reference .in file, thus building the temporary .in file
-  { cat ../input/bismuth_ecut_conv.in; echo "ecut $ecut"; } > $temp_input_file_path
-  #launch abinit with ecut value of current iteration
-  log="log_ecut_$ecut"
+  #add ngkpt value to reference .in file, thus building the temporary .in file
+  { cat ../input/bismuth_kpt_conv.in; echo "ngkpt $ngkpt $ngkpt $ngkpt"; } > $temp_input_file_path
+  #launch abinit with ngkpt value of current iteration
+  log="log_ngkpt_$ngkpt"
   echo "$abinit_files" | abinit >& $log
   #delete temporary .in file
   rm $temp_input_file_path
 
   cur_etotal=$(awk '{for(i=1;i<=NF;i++) if ($i=="etotal") print $(i+1)}' $output_file_path)
   #From scientific notation to decimal, bc does not handle correctly scientific notation
-  cur_etotal=$(python -c "print float('$cur_etotal')")
-
-  bc_arg="$prev_etotal - $cur_etotal < $delta_etotal_conv"
+  cur_etotal=$(python -c "print '%.16f' % float('$cur_etotal')")
+  delta_etotal=$(python -c "print '%.16f' % abs(float('$prev_etotal') - float('$cur_etotal'))")
+  bc_arg="$delta_etotal < $delta_etotal_conv"
   has_converged=$(bc <<< $bc_arg)
 
   if $first_iter;
   then
-    ecut_vec="$ecut"
+    ngkpt_vec="$ngkpt"
     etotal_vec="$cur_etotal"
     first_iter=false
   else
-    ecut_vec="$ecut_vec $ecut"
+    ngkpt_vec="$ngkpt_vec $ngkpt"
     etotal_vec="$etotal_vec $cur_etotal"
   fi
 
-  echo $ecut
+  echo $ngkpt
   echo $cur_etotal
 
   prev_etotal=$cur_etotal
-  ((ecut++))
+  ((ngkpt++))
 
 done
 
@@ -68,14 +68,14 @@ python << END
 
 import matplotlib.pyplot as plt
 
-ecut_vec = map(int, "$ecut_vec".split(' '))
+ngkpt_vec = map(int, "$ngkpt_vec".split(' '))
 etotal_vec= map(float, "$etotal_vec".split(' '))
 
-print(ecut_vec)
+print(ngkpt_vec)
 print(etotal_vec)
 
-plt.plot(ecut_vec, etotal_vec)
-plt.xlabel('ecut')
+plt.plot(ngkpt_vec, etotal_vec)
+plt.xlabel('ngkpt')
 plt.ylabel('etotal')
 plt.show()
 END
