3ef9139e6a0c5c2258b484a7b1a9ab9ea3eb2cc2
Arnaud Schils <arnaud.schils@gmail.com>
2017-04-11 11:39:07 +0200

    automatically write band structure plot to file

--- figures/plot_band_structure.py
+++ figures/plot_band_structure.py
@@ -7,9 +7,11 @@ using the eigenvalues stored in the GSR file produced by abinit at the end of th
 from abipy.abilab import abiopen
 import abipy.data as abidata
 
+project_dir_path = "/Users/aschils/Documents/FYAP/FYAP21MS/Q2/atomistic_and_nanoscopic_simulation/projet/LMAPR2451/"
+
 # Here we use one of the GSR files shipped with abipy.
 # Replace filename with the path to your GSR file or your WFK file.
-filename = abidata.ref_file("/Users/aschils/Documents/FYAP/FYAP21MS/Q2/atomistic_and_nanoscopic_simulation/projet/LMAPR2451/work/tbase1_xo2_GSR.nc")
+filename = abidata.ref_file(project_dir_path+"work/tbase1_xo2_GSR.nc")
 
 # Open the GSR file and extract the band structure.
 # (alternatively one can use the shell and `abiopen.py OUT_GSR.nc -nb` to open the file in a jupyter notebook.
@@ -18,7 +20,7 @@ with abiopen(filename) as ncfile:
 
 # Plot the band energies. Note that the labels for the k-points
 # are found automatically in an internal database.
-ebands.plot(title="Silicon band structure")
+ebands.plot(title="Silicon band structure", savefig=project_dir_path+"figures/band_structure.png", show=False)
 
 # Plot the BZ and the k-point path.
 #ebands.kpoints.plot()
