044731d9e4987891fa0f44c8612167c22a7ff81b
Arnaud Schils <arnaud.schils@gmail.com>
2017-04-15 13:57:19 +0200

    plot band structure with spin orbit

--- work/band.sh
+++ work/band.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-abinit < "../input/bismuth.files" >& log_band
+abinit < "../input/bismuth_so.files" >& log_band
 echo "######### SECOND SIMU ##########" >> log_band
-abinit < "../input/bismuth_band.files" >> log_band
+abinit < "../input/bismuth_band_so.files" >> log_band
 export PYTHONPATH="${PYTHONPATH}/usr/local/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages"
 ./../figures/plot_band_structure.py
